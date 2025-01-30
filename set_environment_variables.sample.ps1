4e6c6aacd57f422784b8d7c6282bfe7854dcfcfe
Ramakrishna Natarajan <nkrama99@gmail.com>
2025-01-30 00:09:50 -0500

    required env variabled

---
+++ set_environment_variables.sample.ps1
@@ -0,0 +1,15 @@
+# Prefect auth
+$env:PREFECT_API_KEY = ""
+$env:PREFECT_CLOUD_ACCOUNT_ID = ""
+
+# AWS auth
+$env:AWS_ACCESS_KEY_ID = ""
+$env:AWS_SECRET_ACCESS_KEY = ""
+$env:AWS_REGION = "us-east-1"
+
+# Terraform variables
+$env:TF_VAR_prefect_workspace_id = ""
+$env:TF_VAR_data_bucket_name = "dummy-data"
+$env:TF_VAR_model_bucket_name = "dummy-model"
+$env:TF_VAR_model_training_deployment_id = ""
+$env:TF_VAR_model_inference_deployment_id = ""
